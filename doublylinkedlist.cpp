b832a4cd119daf2cd1fa266b93b33535858a4177
azadkumaranand <azadanand9798@gmail.com>
2023-09-26 22:17:22 +0530

    code of linkedlist and doublylinkedlist

---
+++ doublylinkedlist.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+
+class DoblyLinkedList{
+    public:
+    Node* head;
+    Node* tail;
+    
+    DoblyLinkedList(){
+        head = nullptr;
+        tail = nullptr;
+    }
+    void insertAtEnd(int data){
+        Node* newNode = new Node(data);
+        if(!tail){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->prev = tail;
+        tail->next  = newNode;
+        tail = newNode;
+
+    }
+    void displayForwardList(){
+        Node* current = head;
+        while(current){
+            cout<<current->data<<" ";
+            current = current->next;
+        }
+    }
+};
+
+int main(){
+    DoblyLinkedList dll;
+    dll.insertAtEnd(1);
+    dll.insertAtEnd(2);
+    dll.insertAtEnd(3);
+    dll.insertAtEnd(4);
+    dll.displayForwardList();
+}
--- linkedlist.cpp
+++ linkedlist.cpp
@@ -2,6 +2,53 @@
 
 using namespace std;
 
-int main(){
+class Node{
+    public:
+    int data;
+    Node* next;
     
+    public:
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+class Linkedlist{
+    public:
+    Node* head;
+    Linkedlist(){
+        head = nullptr;
+    }
+    void insertelement(int data){
+        Node* newNode = new Node(data);
+        if(!head){
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while(current->next){
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    void displaylist(){
+        Node* current = head;
+        while (current)
+        {
+            cout<<current->data<<" ";
+            current = current->next;
+        }
+        
+    }
+};
+
+int main(){
+    Linkedlist my_list;
+    my_list.insertelement(1);
+    my_list.insertelement(2);
+    my_list.insertelement(3);
+    my_list.insertelement(4);
+    my_list.insertelement(5);
+    my_list.insertelement(6);
+    my_list.displaylist();
 }
